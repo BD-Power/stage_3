6c767fff6383c7b5b9b721670ed342c84d68ee32
Agustin-Casebonne <agustincasebonne9@gmail.com>
2025-12-03 13:30:24 +0000

    changes item 1

--- indexer-service/src/main/java/org/example/IndexListener.java
+++ indexer-service/src/main/java/org/example/IndexListener.java
@@ -1,36 +1,68 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
 import com.hazelcast.multimap.MultiMap;
-import es.ulpgc.searchcluster.DocumentEvent;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 @Component
 public class IndexListener {
+
     private final HazelcastInstance hz;
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final IMap<String, Boolean> processedDocuments;
 
-    public IndexListener(HazelcastInstance hz) { this.hz = hz; }
+    public IndexListener(HazelcastInstance hz) {
+        this.hz = hz;
 
-    @JmsListener(destination = "documents.ingested")
-    public void onMessage(DocumentEvent ev) {
+        this.processedDocuments = hz.getMap("processedDocuments");
+    }
+
+    @JmsListener(destination = "document.queue")
+    public void onMessage(String messageJson) {
         try {
-            String content = Files.readString(Path.of(ev.getPath()));
-            indexDocument(ev.getDocId(), content);
+            @SuppressWarnings("unchecked")
+            Map<String, Object> msg = mapper.readValue(messageJson, Map.class);
+
+            String docId = (String) msg.get("documentId");
+            String ruta = (String) msg.get("ruta");
+
+
+            if (processedDocuments.putIfAbsent(docId, Boolean.TRUE) != null) {
+                System.out.println("Duplicado detectado: El ID " + docId + " ya fue procesado. Ignorando.");
+                return;
+            }
+
+
+            System.out.println("📩 Mensaje recibido en INDEXER");
+            System.out.println(" - ID:   " + docId);
+            System.out.println(" - Ruta: " + ruta);
+
+            String content = Files.readString(Path.of(ruta));
+
+            indexDocument(docId, content);
+
         } catch (Exception e) {
             e.printStackTrace();
+
         }
     }
 
     private void indexDocument(String docId, String content) {
-        MultiMap<String,String> inverted = hz.getMultiMap("inverted-index");
+        MultiMap<String, String> inverted = hz.getMultiMap("inverted-index");
+
         String[] tokens = content.toLowerCase().split("\\W+");
-        for (String t: tokens) {
-            if (t == null || t.isBlank()) continue;
-            inverted.put(t, docId);
+        for (String token : tokens) {
+            if (token == null || token.isBlank()) continue;
+            inverted.put(token, docId);
         }
+
+        System.out.println("Documento indexado en MultiMap: " + docId);
     }
 }
